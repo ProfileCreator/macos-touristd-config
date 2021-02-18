fafb635729f214ca599e981f728a5aeccd547484
Stephen Warneford-Bygrave <sbygrave@mac.com>
2021-02-18 17:07:07 +0000

    Added -k switch to curl

--- DownloadTouristDConfig.sh
+++ DownloadTouristDConfig.sh
@@ -4,4 +4,4 @@ TOURISTD_URL="https://help.apple.com/macOS/config.json"
 SCRIPT_DIRECTORY="$(/usr/bin/dirname "$0")"
 
 echo "Downloading latest touristd config.json..."
-/usr/bin/curl "${TOURISTD_URL}" -o "${SCRIPT_DIRECTORY}/config.json"
+/usr/bin/curl -k "${TOURISTD_URL}" -o "${SCRIPT_DIRECTORY}/config.json"
